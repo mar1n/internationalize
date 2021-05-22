d022639b582d4ce3843504e90c3920b51a6bf00c
mar1n <30738216+mar1n@users.noreply.github.com>
2021-05-22 22:28:59 +0100

    integratio with locize service

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import { useTranslation, Trans } from 'react-i18next';
-import { useState, Suspense } from 'react';
-import Footer from './Footer';
-const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' }
-};
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation, Trans } from "react-i18next";
+import { useState, Suspense, useEffect } from "react";
+import Footer from "./Footer";
 
 function App() {
   const { t, i18n } = useTranslation();
   const [count, setCounter] = useState(0);
+
+  const [lngs, setLngs] = useState({ en: { nativeName: "English" } });
+
+  useEffect(() => {
+    i18n.services.backendConnector.backend.getLanguages((err, ret) => {
+      if (err) return console.log('err', err);
+      setLngs(ret);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {i18n.changeLanguage(lng); setCounter(count + 1);}}>
+            <button
+              key={lng}
+              style={{ fontWeight: i18n.language === lng ? "bold" : "normal" }}
+              type="submit"
+              onClick={() => {
+                i18n.changeLanguage(lng);
+                setCounter(count + 1);
+              }}
+            >
               {lngs[lng].nativeName}
             </button>
           ))}
         </div>
         <p>
-          <i>{t('counter', { count })}</i>
+          <i>{t("counter", { count })}</i>
         </p>
         <p>
           <Trans i18nKey="description.part1">
-          Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
           </Trans>
         </p>
         <a
@@ -36,16 +50,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {t('description.part2')}
+          {t("description.part2")}
         </a>
       </header>
-      <Footer t={t}/>
+      <Footer t={t} />
     </div>
   );
 }
 
-
-
+// here app catches the suspense from page in case translations are not yet loaded
 export default function WrappedApp() {
   return (
     <Suspense fallback="...is loading">
--- src/i18n.js
+++ src/i18n.js
@@ -1,63 +1,59 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import { DateTime } from "luxon";
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-locize-backend';
+import LastUsed from 'locize-lastused';
+import { locizePlugin } from 'locize';
+import { DateTime } from 'luxon';
+
+const isProduction = 'production';
+
+const locizeOptions = {
+  projectId: '8443747a-674e-4c1d-afe1-4d10e706d9e9',
+  apiKey: 'd2cd0f1d-fd2c-41fa-9513-06ac4e6c2d13', 
+  referenceLng: 'en',
+  version: process.env.REACT_APP_LOCIZE_VERSION
+};
+
+if (!isProduction) {
+  // locize-lastused
+  // sets a timestamp of last access on every translation segment on locize
+  // -> safely remove the ones not being touched for weeks/months
+  // https://github.com/locize/locize-lastused
+  i18n.use(LastUsed);
+}
 
 i18n
+  // locize-editor
+  // InContext Editor of locize
+  .use(locizePlugin)
+  // i18next-locize-backend
+  // loads translations from your project, saves new keys to it (saveMissing: true)
+  // https://github.com/locize/i18next-locize-backend
   .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: "en",
+    fallbackLng: 'en',
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
       format: (value, format, lng) => {
         if (value instanceof Date) {
-          return DateTime.fromJSDate(value)
-            .setLocale(lng)
-            .toLocaleString(DateTime[format]);
+          return DateTime.fromJSDate(value).setLocale(lng).toLocaleString(DateTime[format])
         }
         return value;
-      },
+      }
     },
-    resources: {
-      en: {
-        translation: {
-          description: {
-            part1: "Edit <1>src/App.js</1> and save to reload.",
-            part2: "Learn React",
-          },
-          counter: "Changed language just once",
-          counter_plural: "Changed language already{{count}} times",
-          footer: {
-            date: "Today is {{date, DATE_HUGE}}",
-            date_morning:
-              "Good morning! Today is {{date, DATE_HUGE}} | Have a nice day!",
-            date_afternoon: "Good afternoon! It's {{date, DATE_HUGE}}",
-            date_evening: "Good evening! Today was the {{date, DATE_HUGE}}",
-          },
-        },
-      },
-      de: {
-        translation: {
-          description: {
-            part1: "Ändere <1>src/App.js</1> und speichere um neu zu laden.",
-            part2: "Lerne React",
-          },
-          counter: "Die Sprache wurde erst ein mal gewechselt",
-          counter_plural: "Die Sprache wurde {{count}} mal gewechselt",
-          footer: {
-            date: "Heute ist {{date, DATE_HUGE}}",
-            date_morning:
-              "Guten Morgen! Heute ist {{date, DATE_HUGE}} | Wünsche einen schönen Tag!",
-            date_afternoon: "Guten Tag! Es ist {{date, DATE_HUGE}}",
-            date_evening: "Guten Abend! Heute war {{date, DATE_HUGE}}",
-          },
-        },
-      },
-    },
+    backend: locizeOptions,
+    locizeLastUsed: locizeOptions,
+    saveMissing: !isProduction // you should not use saveMissing in production
   });
 
 export default i18n;
